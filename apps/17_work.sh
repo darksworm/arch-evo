b6464ab5881a81fa8c93fb21ec2a1235bc649fa6
Ilmars Janis Bluzmanis <132436248+ilmarspenneo@users.noreply.github.com>
2026-03-07 18:43:04 +0100

    Fix Linear package name: linear-app → linear-desktop-bin

--- apps/17_work.sh
+++ apps/17_work.sh
@@ -8,6 +8,6 @@ section "Work Tools"
 pac_install libreoffice-fresh networkmanager-openvpn
 
 ensure_yay
-yay_install slack-desktop kubectl-argo-rollouts-bin kubelogin pritunl-client-bin linear-app
+yay_install slack-desktop kubectl-argo-rollouts-bin kubelogin pritunl-client-bin linear-desktop-bin
 
 log "Work tools installed"
