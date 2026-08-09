822827dd3e0b5502a60c4ab588a531f684af882b
Ilmars Janis Bluzmanis <9987548+darksworm@users.noreply.github.com>
2026-08-09 18:40:15 +0200

    feat: bluetooth-ui wrapper to tame blueman applet/tray lifecycle

--- apps/06_bluetooth.sh
+++ apps/06_bluetooth.sh
@@ -9,4 +9,7 @@ pac_install bluez bluez-utils blueman
 
 enable_service bluetooth.service
 
+# blueman's AutoConnect races bluetoothd's own reconnect and drops the device
+gsettings set org.blueman.general plugin-list "['!AutoConnect']"
+
 log "Bluetooth installed and enabled"
